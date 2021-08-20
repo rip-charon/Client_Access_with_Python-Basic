c32e4d9bb1f5ab0446307369d30e59de67e47fb9
CHARON <rip.charon@gmail.com>
2021-08-20 08:50:52 -0800

    add web page reader python code with send and receive

---
+++ part-01/Request_Connection_send_and_recv_data_02.py
@@ -0,0 +1,17 @@
+import socket
+
+s = socket.socket(socket.AF_INET , socket.SOCK_STREAM) # tcp - connection
+
+s.connect(('172.217.19.174',80))
+
+print("\n[+] connected.")
+
+s.send("GET /index.php HTTP/1.0\n\n".encode())
+
+data = s.recv(2048)
+
+print("[+] here is a data : \n")
+
+print(data)
+
+s.close()
