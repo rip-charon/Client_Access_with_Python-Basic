252c1241bda0b2fdd610238c65c9a33899a68950
charon <rip.charon@gmail.com>
2021-08-21 22:21:32 +0430

    done codeing basic server side

--- part-02/Create_Server_file_and_Client_01.py
+++ part-02/Create_Server_file_and_Client_01.py
@@ -1,2 +1,24 @@
 # CHARON :
 # socket program fot creating server on port 2323 and send and recive data from client side.
+
+from socket import *
+
+s = socket(AF_INET,SOCK_STREAM)
+
+s.bind(("192.168.1.115",2323))
+
+s.listen(5)
+
+print("[+] server start running on port 2323 \n")
+
+client , addr = s.accept()
+
+print("connected to %s \n " % str(addr))
+
+client.sendall("hello i am server... ")
+
+data = client.recv(1024)
+
+print(data)
+
+client.close()
