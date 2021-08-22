1d99ec4ef968c251f2f3f7e9c9e927b1609d42de
CHARON <rip.charon@gmail.com>
2021-08-22 01:18:02 -0800

    code client side for udp server

---
+++ part-03/Create UDP_Connection_send_and_recv_data_02.py
@@ -0,0 +1,10 @@
+# CHARON
+# socket program for creating UDP client for send and recive data
+
+from socket import *
+
+s = socket(AF_INET , SOCK_DGRAM)
+
+s.sendto("[-] client >> hello i am the udp client ".encode('utf-8'),("192.168.1.15",5050))
+
+s.close()
