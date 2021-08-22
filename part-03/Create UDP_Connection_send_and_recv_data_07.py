163f32de11508b1d87c19cdcfe2975bcbcce2256
CHARON <rip.charon@gmail.com>
2021-08-22 01:59:16 -0800

    code port scanner with an other way

---
+++ part-03/Create UDP_Connection_send_and_recv_data_07.py
@@ -0,0 +1,24 @@
+# CHARON
+# socket program for scan ip address for checking close or open port.
+
+from socket import *
+
+while True:
+
+    s = socket(AF_INET,SOCK_STREAM)
+
+    ip = input("[+] ip : ")
+
+    port = int(input("[+] port : "))
+
+    r = s.connect_ex((ip,port))
+
+    if r == 0 :
+
+        print("[-] check >> open port")
+
+    else :
+
+        print("[-] check >> close port")
+
+s.close()
