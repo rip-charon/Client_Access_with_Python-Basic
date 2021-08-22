d2bf83f264f8624ba1ef7c3a6209602c36c87286
CHARON <rip.charon@gmail.com>
2021-08-22 01:48:20 -0800

    adding more send and recive data

---
+++ part-03/Create UDP_Connection_send_and_recv_data_05.py
@@ -0,0 +1,28 @@
+# CHARON
+# socket program for creating UDP server for send and recive data
+
+from socket import *
+
+s = socket(AF_INET , SOCK_DGRAM)
+
+server = "0.0.0.0"
+
+port = 5050
+
+ADDR = (server,port)
+
+s.bind((ADDR))
+
+print("[+] server is running on port 5050 \n")
+
+c , adrr = s.recvfrom(1025)
+
+print("[+] connected to %s \n" % str(adrr))
+
+print("[+] recived : %s " % str(c))
+
+s.sendto("hello i am server !".encode('utf-8'), adrr)
+
+print(s.recv(1024))
+
+s.close()
