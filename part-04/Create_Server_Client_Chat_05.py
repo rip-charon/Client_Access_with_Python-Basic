3a9b2ce897082e8f5b3f709bf576c64de2137520
CHARON <rip.charon@gmail.com>
2021-08-22 04:37:12 -0800

    rewrite tcp chat server code to udp chat server

---
+++ part-04/Create_Server_Client_Chat_05.py
@@ -0,0 +1,34 @@
+# CHARON
+# socket program for creating server on port 5050 for chatting in udp connection with client
+
+from socket import *
+
+s = socket(AF_INET , SOCK_DGRAM)
+
+server = "0.0.0.0"
+
+port = 5050
+
+ADDR = (server,port)
+
+s.bind((ADDR))
+
+print("[+] server chat is running : ")
+
+name = input("[+] Enter your name : ")
+
+c , ADDR2 = s.recvfrom(1024)
+
+print("[+] connected to %s \n" % str(ADDR2))
+
+while True:
+
+    sms = input("[+] you >> ")
+    cms = "[+] %s >> %s" % (name,sms)
+    s.sendto(sms.encode('utf-8'),ADDR2)
+    print("\n")
+    data = c.recv(1024)
+    print(data.decode('utf-8'))
+    print("\n")
+
+c.close()
---
+++ part-04/Create_Server_Client_Chat_06.py
@@ -0,0 +1,24 @@
+# CHARON :
+# socket program for creating client on port 5050 for chatting in udp connection with server
+
+from socket import *
+
+print("[+] client chat is running : ")
+
+name = input("[+] Enter your name : ")
+
+s = socket(AF_INET, SOCK_DGRAM)
+
+print("connected to server \n")
+
+while True:
+
+    data = s.recv(1024)
+    print(data.decode('utf-8'))
+    print("\n")
+    sms = input("[+] you >> ")
+    cms = "[+] %s >> %s}" % (name,sms)
+    s.sendto(cms.encode('utf-8'),("192.168.1.123",5050))
+    print("\n")
+
+s.close()
