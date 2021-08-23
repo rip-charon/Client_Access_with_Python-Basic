ae0e233da541f76276b0f543ff23dfb6e6283c2b
CHARON <rip.charon@gmail.com>
2021-08-23 00:35:27 -0800

    codeing basic client side done

---
+++ part-06/Send_data_in_Network_with_Encryption_01.py
@@ -0,0 +1,21 @@
+# CHARON :
+# socket program for creating client on port 5050 for chatting in tcp connection with server
+
+from socket import *
+
+s = socket(AF_INET, SOCK_STREAM)
+
+s.connect(("192.168.1.115",5050))
+
+print("connected to server \n")
+
+while True:
+
+    data = s.recv(1024)
+    print("[+] person >> %s " % data.decode('utf-8'))
+    print("\n")
+    sms = input("[+] you >> ")
+    s.sendall(sms.encode('utf-8'))
+    print("\n")
+
+s.close()
